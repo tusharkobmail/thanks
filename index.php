76e6e9d5acdaa962b52d45793ce9fcc7027174f4
tusharkobmail <73995885+tusharkobmail@users.noreply.github.com>
2020-11-05 19:22:00 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+function calcHeight(iframeElement){
+    var the_height=  iframeElement.contentWindow.document.body.scrollHeight;
+    iframeElement.height=  the_height;
+}
+</script>
+<title>      Easy Thanksgiving Decor Ideas    </title>
+<iframe src="https://www.redbookmag.com/home/decor/g3681/easy-thanksgiving-decor/" onLoad="calcHeight(this);" frameborder="0" scrolling="no" id="the_iframe" width="100%" ></iframe>
